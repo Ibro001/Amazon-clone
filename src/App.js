4fe27d0a9f2b8850f03da104227d7001b11b5199
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-21 19:51:05 +0100

    created routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import styled from "styled-components";
 
 function App() {
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <Router>
+      <Container>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<h1>Login Page</h1>} />
+          <Route path="/signup" element={<h1>SignUp Page</h1>} />
+        </Routes>
+      </Container>
+    </Router>
+    
   );
 }
 
