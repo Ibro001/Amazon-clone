cd61cfe44443b13a3c61cebea50802638bc69933
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-25 18:37:42 +0100

    created the home page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import styled from "styled-components";
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <Router>
       <Container>
         <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={< Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
---
+++ src/Components/Home.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import styled from 'styled-components'
+import Card from './Card';
+import Navbar from './Navbar';
+
+function Home() {
+  return (
+    <Container>
+      <Navbar />
+      <Banner>
+        <img src="./banner.jpg" alt="background_banner" />
+        <img src="mobile_banner.jpg" alt="mobile_banner_background" />
+      </Banner>
+      <Main>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Main>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    width: 100%;
+    background-color: rgb(234, 237, 237);
+`;
+
+const Banner = styled.div`
+    width: 100%;
+
+    img{
+        width: 100%;
+        -webkit-mask-image: linear-gradient(
+            to bottom,
+            rgba(0, 0, 0, 2),
+            rgba(0, 0, 0, 0.95),
+            rgba(0, 0, 0, 0.85),
+            rgba(0, 0, 0, 0.75),
+            rgba(0, 0, 0, 0.55),
+            rgba(0, 0, 0, 0)
+        );
+
+        &:nth-child(2){
+            display: none;
+        }
+
+        @media only screen and (max-width: 767px){
+            &:nth-child(1){
+                display: none;
+            }
+            &:nth-child(2){
+                display: block;
+            }
+            -webkit-mask-image: none;
+        }
+    }
+`;
+
+const Main = styled.div`
+    display: grid;
+    justify-content: center;
+    place-items: center;
+    width: 100%;
+
+    grid-auto-rows: 420px;
+    grid-template-columns: repeat(4, 280px);
+    grid-gap: 20px;
+
+    /* mobile screen */
+    @media only screen and (max-width: 767px){
+        grid-template-column: repeat(2, 50%);
+        grid-gap: 0;
+    }
+
+    /* Tablets */
+
+  @media only screen and (min-width: 767px) and (max-width: 1200px) {
+    grid-template-columns: repeat(3, 30%);
+  }
+
+    /*Desktop Browser*/
+    @media only screen and (min-width: 767px) {
+        margin-top: -130px;
+        padding: 10px 0px;
+    }
+`;
+
+export default Home
