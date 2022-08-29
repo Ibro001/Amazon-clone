5ff657b3bda43811a5a93a0ee8e88d7a9a4cdb1b
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-29 13:49:54 +0100

    created the case for removing a product from the basket

--- src/Components/reducer.js
+++ src/Components/reducer.js
@@ -1,20 +1,41 @@
 export const initialState = {
     basket: [],
-    User: null
+    user: null
 };
+
+export const getBasketTotal = (basket) => 
+basket.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
     console.log('action>>>>', action);
-
     switch(action.type) {
-        case 'ADD_TO_CART':
-            return{
+        case 'ADD_TO_BASKET':
+            return {
                 ...state,
                 basket: [...state.basket, action.item],
             };
+        case 'REMOVE_FROM_BASKET':
+
+        const index = state.basket.findIndex(          /**find the index of the item we wish to delete */
+            (basketItem) => basketItem.id === action.id
+        );
+
+        let newBasket = [...state.basket]; /**save the new basket value */
+
+        if(index >= 0){
+            newBasket.splice(index,1)
+        }else{
+            console.warn(`can't remove product whose id is ${index}`);
+        }
+            
+        
+        return {
+                ...state,
+                basket: newBasket,
+            };
 
             default:
-                return state
+                return state;
     }
 }
 
