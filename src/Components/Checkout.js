aefbbd6f190413397db7d461e94673febc888d1b
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-29 13:43:58 +0100

    set up the checkout page and the remove button for removing products

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,16 +1,203 @@
 import React from 'react'
 import { useStateValue } from '../Context/StateProvider'
+import styled from 'styled-components';
+import NavBar from './Navbar'
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
 
 function Checkout() {
 
-    const [{ basket }] = useStateValue();
-    console.log('checkout>>>', basket);
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = (e, id) => {
+    e.preventDefault();
+
+    dispatch({
+      type: 'REMOVE_FROM_BASKET',
+      id: id,
+    });
+  };
+    
 
   return (
-    <div>
-      <h1>checkout</h1>
-    </div>
+    <Container>
+      <NavBar />
+
+      <Main>
+        <ShoppingCart>
+          <h2>Shopping Cart</h2>
+
+          {
+            basket?.map((product) => (
+              <Product>
+
+              <Image>
+                <img src={product.image} alt='echoe_img' />
+              </Image>
+
+              <Description>
+                <h4>{product.title}</h4>
+                <p><sup>$</sup>{product.price}<sup>99</sup></p>
+                <button onClick={(e) => removeFromBasket(e, product.id)}>Remove</button>
+              </Description>
+
+          </Product>
+            ))
+          }
+
+          
+
+        </ShoppingCart>
+
+        <Subtotal>
+          <CurrencyFormat 
+            renderText={value => (
+              <>
+
+                <p>
+                  Subtotal ({basket.length} items ) : <strong>{value}</strong>
+                </p>
+                <small>
+                  <input type='checkbox'/>
+                  <span>
+                    This order contains a gift.
+                  </span>
+                </small>
+              </>
+            )}
+            decimalScale={2}
+            value={getBasketTotal(basket)}
+            displayType='text'
+            thousandSeparator={true}
+            prefix={'$'}
+          />
+
+          <button>Proceed to checkout</button>
+
+        </Subtotal>
+
+      </Main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1400%;
+  height: fit-content;
+  margin: auto;
+  background-color: rgb(234, 237, 237);
+  border: 1px solid red;
+  position: relative;
+`;
+
+const Main = styled.div`
+  display: flex;
+  padding: 15px;
+
+  @media only screen and (max-width: 1200px){
+    flex-direction: column;
+  }
+`;
+
+const ShoppingCart = styled.div`
+  padding: 15px;
+  background-color: #fff;
+  flex: 0.7;
+
+  @media only screen and (max-width: 1200px){
+    flex: none;
+  }
+
+  h2{
+    font-weight: 500;
+    border-bottom: 1px solid lightgray;
+    padding-bottom: 15px;
+
+  }
+`;
+
+const Subtotal = styled.div`
+  flex: 0.3;
+  background-color: #fff;
+  margin-left:15px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+  @media only screen and (max-width: 1200px){
+    flex: none;
+    margin-top: 20px;
+  }
+
+  p{
+    font-size: 20px;
+  }
+
+  small{
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+
+    span{
+      margin-left: 10px;
+    }
+  }
+
+  button{
+    width: 65%;
+    height: 40px;
+    margin-top: 10px;
+    background-color: #ffd814;
+    border: none;
+    outline: none;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
+
+const Product = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Image = styled.div`
+  img{
+    margin-top: 10px;
+    width: 100%;
+  }
+`;
+
+const Description = styled.div`
+  flex: 0.7;
+
+  h4{
+    font-weight: 600;
+    font-size: 18px;
+    margin-left: 10px;
+  }
+
+  p{
+    font-weight: 600;
+    margin-top: 10px;
+    margin-left: 10px;
+  }
+
+  button{
+    background-color: transparent;
+    color: #1384b4;
+    border: none;
+    outline: none;
+    margin-top: 10px;
+    cursor: pointer;
+    margin-left: 10px;
+
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+`;
 
 export default Checkout
