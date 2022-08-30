d2ee190f07f43a0f9f669781eced158014f04a19
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-30 20:43:09 +0100

    stripe setup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Home from "./Components/Home";
 import Checkout from "./Components/Checkout";
 import Address from "./Components/Address";
 import Payment from "./Components/Payment";
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
+
+const promise = loadStripe(
+  'pk_test_51LcVfmC6XiTjG1nLey4eZZ2aK3yjYUBqXwNtMKYpaepsojcZCw9tQi3CRgMMfA9WiiYbHcTfHW1GMoI4o1ufZsq1000PWxZCW1'
+)
 
 function App() {
   return (
@@ -18,7 +24,14 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/checkout" element={<Checkout />} /> 
           <Route path="/address" element={<Address />} />
-          <Route path="/payment" element={<Payment />} />
+          <Route 
+            path="/payment" 
+            element={
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>
+            } 
+          />
         </Routes>
       </Container>
     </Router>
