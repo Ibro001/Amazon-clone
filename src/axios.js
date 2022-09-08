dc1adefb3f6e9d145a667c7191ab4797f267355c
Ibro001 <ibrahimmoses56@gmail.com>
2022-09-08 17:48:27 +0100

    replaced axios URL with heroku URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://amazon-clone-backend2.herokuapp.com'  /**Replace our axios URL i.e(http://localhost:3001) with the Heroku URL generated from deploying on Heroku */
 });
 
 export default instance;
